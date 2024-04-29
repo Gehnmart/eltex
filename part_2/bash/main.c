2ea5837e577ccc85295c1fd56eb22aafcbc63803
gehnmart <gehnmart>
2024-04-29 22:13:48 +0700

    bash

--- part_2/bash/main.c
+++ part_2/bash/main.c
@@ -1,10 +1,12 @@
-#include <stdio.h>
 #include <linux/limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
+
+#define ARGV_MAX 256
 
 void CreateArgv(char *str, char **argv) {
   int index = 0;
@@ -12,34 +14,38 @@ void CreateArgv(char *str, char **argv) {
   char *ptr = strtok_r(str, " ", &save);
   do {
     argv[index++] = ptr;
-  } while((ptr = strtok_r(NULL, " ", &save)) != NULL);
+  } while ((ptr = strtok_r(NULL, " ", &save)) != NULL);
 }
 
 void RunNewProcess(const char *path, char **argv) {
   int status = 0;
   int child_proc = fork();
-  if(!child_proc) {
+  if (!child_proc) {
     execv(path, argv);
     fprintf(stderr, "%s", path);
     perror("");
     exit(EXIT_FAILURE);
   }
-  if(wait(&status) == -1){
+  if (wait(&status) == -1) {
     perror("wait error");
+    exit(EXIT_FAILURE);
   }
 }
 
 int main() {
   char buf[PATH_MAX];
-  while(1){
+  char *argv[ARGV_MAX] = {0};
+  while (1) {
     fgets(buf, PATH_MAX, stdin);
     buf[strlen(buf) - 1] = '\0';
 
-    if(strcmp(buf, "exit") == 0){
+    if (strcmp(buf, "exit") == 0) {
       exit(EXIT_SUCCESS);
     }
-    char *argv[512] = {0};
+
     CreateArgv(buf, argv);
     RunNewProcess(buf, argv);
+
+    memset(argv, 0, sizeof(char *) * ARGV_MAX);
   }
 }
