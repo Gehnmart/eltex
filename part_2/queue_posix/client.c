d154e13b4b6c1224d1f98ac0a0250cf817631ea6
gehnmart <gehnmart>
2024-05-02 05:00:59 +0700

    Added queue posix and system v

---
+++ part_2/queue_posix/client.c
@@ -0,0 +1,36 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <mqueue.h>
+
+#define BUF_MAX 256
+
+int main() {
+  int msqid;
+  mqd_t mqd;
+  unsigned int prior = 1;
+
+  struct mq_attr attr = {0, 10, BUF_MAX, 0};
+
+  mqd = mq_open("/my_queue", O_CREAT | O_RDWR, 0666, &attr);
+  if(mqd < 0){
+    perror("mq_open");
+    exit(EXIT_FAILURE);
+  }
+
+  char buf[BUF_MAX] = {0};
+  mq_receive(mqd, buf, BUF_MAX, &prior);
+  printf("%s", buf);
+
+  mq_send(mqd, "Hello!", 7, prior);
+
+  mq_close(mqd);
+  mq_unlink("/my_queue");
+
+  exit(EXIT_SUCCESS);
+}
---
+++ part_2/queue_system_v/client.c
@@ -0,0 +1,40 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+typedef struct {
+  long mtype;
+  char mtext[64];
+} Message;
+
+int main() {
+  int msqid;
+  key_t key;
+
+  if((key = ftok("server.c", 'B')) < 0) {
+    perror("ftok error");
+    exit(EXIT_FAILURE);
+  }
+
+  if((msqid = msgget(key, IPC_CREAT|0666)) < 0) {
+    perror("msgget error");
+    exit(EXIT_FAILURE);
+  }
+  
+  Message msgr = {0};
+  msgrcv(msqid, &msgr, sizeof(msgr), 1, 0);
+  printf("%s\n", msgr.mtext);
+
+  Message msgs = {1, "Hello!"};
+  msgsnd(msqid, &msgs, sizeof(msgs), 0);
+
+  msgctl(msqid, IPC_RMID, NULL);
+  
+  exit(EXIT_SUCCESS);
+}
