8878dad3ee072da65b6f29cc07c7e322cf938c7e
gehnmart <dimaniji@outlook.com>
2024-07-09 08:25:26 +0000

    ex2 complete

--- part_2/socket/ex4/ex2/client.c
+++ part_2/socket/ex4/ex2/client.c
@@ -1,15 +1,20 @@
-#include <string.h>
 #include <arpa/inet.h>
-#include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdlib.h>
-#include <sys/types.h>
+#include <netinet/ip.h>
+#include <netinet/udp.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 #define PORT 2048
 #define PORT_CL 2049
 #define ADDR "127.0.0.1"
+
+#define BF_LEN 64
+#define DATA_LEN (BF_LEN - 8)
 
 #define handle_error(msg) \
   do {                    \
@@ -17,44 +22,47 @@
     exit(EXIT_FAILURE);   \
   } while (0)
 
-typedef union {
-  char  data[64];
-  struct {
-    short src_port;
-    short dst_port;
-    short len;
-    short checksum;
-    char  payload[56];
-  } part;
-} UdpHeader;
-
 int main() {
   struct sockaddr_in serv;
   socklen_t sock_size = sizeof(serv);
-  char buf[50] = {0};
+  char send_buf[BF_LEN] = {0};
+  char recv_buf[BF_LEN] = {0};
   int sfd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP);
-  if (sfd == 1) {
+  if (sfd == -1) {
     handle_error("socket():");
   }
 
-  UdpHeader header;
-  memset(&header, 0, sizeof(header));
-  header.part.src_port = htons(PORT_CL);
-  header.part.dst_port = htons(PORT);
-  header.part.len =      htons(sizeof(header));
-  strncpy(header.part.payload, "Hello world!", sizeof(header.part.payload));
+  struct udphdr udph;
+  memset(&udph, 0, sizeof(udph));
+  udph.source = htons(PORT_CL);
+  udph.dest = htons(PORT);
+  udph.len = htons(sizeof(send_buf));
+  memcpy(send_buf, &udph, sizeof(udph));
+  strncpy(send_buf + sizeof(udph), "HelloWorld!",
+          sizeof(send_buf) - sizeof(udph));
 
+  memset(&serv, 0, sizeof(serv));
   serv.sin_family = AF_INET;
-  serv.sin_addr.s_addr = inet_addr(ADDR);
   serv.sin_port = htons(PORT);
+  inet_pton(AF_INET, ADDR, &serv.sin_addr.s_addr);
 
-  sendto(sfd, &header, sizeof(header), 0, (struct sockaddr *) &serv, sock_size);
-  while(1) {
-    recvfrom(sfd, &header, sizeof(header), 0, (struct sockaddr *) &serv, &sock_size);
-    if(header.part.src_port == htons(PORT)){
-      printf("success\n");
+  while (1) {
+    printf("type your message or type exit:\n");
+    scanf("%55s", send_buf + sizeof(udph));
+    if (strncmp(send_buf + sizeof(udph), "exit", 5) == 0) {
+      break;
     }
-    usleep(100);
+    sendto(sfd, &send_buf, sizeof(send_buf), 0, (struct sockaddr *)&serv,
+           sock_size);
+    while (1) {
+      recvfrom(sfd, &recv_buf, sizeof(recv_buf), 0, (struct sockaddr *)&serv,
+               &sock_size);
+      memcpy(&udph, recv_buf + sizeof(struct iphdr), sizeof(udph));
+      if (udph.source == htons(PORT)) {
+        printf("%s\n", recv_buf + (sizeof(udph) + sizeof(struct iphdr)));
+        break;
+      }
+    }
   }
 
   close(sfd);
--- part_2/socket/ex4/ex2/server.c
+++ part_2/socket/ex4/ex2/server.c
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,30 +17,59 @@
     exit(EXIT_FAILURE);   \
   } while (0)
 
-int main() {
-  int sfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sfd == -1) handle_error("socket");
+typedef struct {
+  int stop;
+  int sfd;
+  struct sockaddr_in addr;
+} ServInfo;
 
-  int coking_addr = 0;
-  inet_pton(AF_INET, ADDR, &coking_addr);
-  struct sockaddr_in serv, client;
-  memset(&serv, 0, sizeof(serv));
+int stop = 0;
+
+void *ServThread(void *argv) {
+  ServInfo *serv = (ServInfo *)argv;
+  struct sockaddr_in client;
   memset(&client, 0, sizeof(client));
-  serv.sin_family = AF_INET;
-  serv.sin_port = htons(PORT);
-  serv.sin_addr.s_addr = coking_addr;
 
-  if (bind(sfd, (struct sockaddr *)&serv, sizeof(serv)) == -1)
-    handle_error("bind");
+  if (bind(serv->sfd, (struct sockaddr *)&serv->addr, sizeof(serv->addr)) == -1)
+    handle_error("bind():");
 
-  char buf[1024] = {0};
+  char recv_buf[64] = {0};
+  char send_buf[64] = {0};
   socklen_t cl_size = sizeof(client);
-  recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&client, &cl_size);
-  printf("packet %s\n", buf);
-  strncpy(buf, "Hello!", sizeof(buf));
-  sendto(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&client, cl_size);
-  recvfrom(sfd, buf, sizeof(buf), 0, (struct sockaddr *)&client, &cl_size);
 
-  close(sfd);
+  while (!stop) {
+    recvfrom(serv->sfd, recv_buf, sizeof(recv_buf), 0,
+             (struct sockaddr *)&client, &cl_size);
+    printf("packet %s\n", recv_buf);
+    strncpy(send_buf, recv_buf, sizeof(recv_buf));
+    sendto(serv->sfd, send_buf, sizeof(recv_buf), 0, (struct sockaddr *)&client,
+           cl_size);
+  }
+
+  close(serv->sfd);
+  pthread_exit(EXIT_SUCCESS);
+}
+
+int main() {
+  ServInfo serv;
+  serv.sfd = socket(AF_INET, SOCK_DGRAM, 0);
+  if (serv.sfd == -1) handle_error("socket");
+
+  memset(&serv.addr, 0, sizeof(serv.addr));
+  serv.addr.sin_family = AF_INET;
+  serv.addr.sin_port = htons(PORT);
+  inet_pton(AF_INET, ADDR, &serv.addr.sin_addr.s_addr);
+
+  pthread_t serv_thread;
+  if (pthread_create(&serv_thread, NULL, ServThread, (void *)&serv) == -1) {
+    handle_error("pthread_create():");
+  }
+  while (getchar() == '0');
+  stop = 1;
+  sendto(serv.sfd, NULL, 0, 0, (struct sockaddr *)&serv.addr,
+         sizeof(serv.addr));
+
+  pthread_join(serv_thread, NULL);
+
   exit(EXIT_SUCCESS);
 }
